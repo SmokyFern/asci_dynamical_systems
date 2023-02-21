a107ec5899ceffa8d8200ebf23376e89eb6a3d41
Denis Vishnyakov <smokyfern@gmail.com>
2023-02-22 00:42:59 +0500

    Remove z-buffer, cosmetic changes to code, add ability to choose asci symbols

--- include/asci_plotter.hpp
+++ include/asci_plotter.hpp
@@ -8,7 +8,7 @@
 class ASCIPlotter {
 
     public:
-        ASCIPlotter(int canvas_width, int canvas_length);
+        ASCIPlotter(std::string list_of_used_characters, int canvas_width, int canvas_length);
 
         void provide_data_3d(const Eigen::MatrixXd &data_3d);
         void provide_data_2d(const Eigen::MatrixXd &data_2d);
@@ -17,24 +17,24 @@ class ASCIPlotter {
                                                                 double y_scale,
                                                                 double depth_offset);
     private:
-        // void compute_projection(Eigen::MatrixXd data_3d, double x_scale, 
-        //                                                  double y_scale,
-        //                                                  double depth_offset);
+        // void compute_projection(const Eigen::MatrixXd &data_3d, double x_scale, 
+        //                                                         double y_scale,
+        //                                                         double depth_offset);
+        void create_rotation_matrix(double alpha, double beta, double gamma);
         Eigen::MatrixXd rotate3d(const Eigen::MatrixXd &data_3d);
         Eigen::MatrixXd rotate2d(const Eigen::MatrixXd &data_2d);
     
     private:
+        Eigen::Matrix3d m_R3d;
+        Eigen::Matrix2d m_R2d;
         Eigen::Vector3d m_light_direction = {0., 1., 1.};
 
-        int m_elements_number;
+        std::string m_used_symbols;
+        int m_idx_coeff;
+
         char m_output[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE];
-        double m_zbuffer[MAX_ARRAY_SIZE][MAX_ARRAY_SIZE];
 
-        Eigen::MatrixXd m_plotting_data;
-        Eigen::VectorXi m_x_proj;
-        Eigen::VectorXi m_y_proj;
-        Eigen::VectorXd m_ooz;
-
+        int m_elements_number;
         int m_canvas_width;
         int m_canvas_height;
 
--- src/asci_plotter.cpp
+++ src/asci_plotter.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "asci_plotter.hpp"
 
-ASCIPlotter::ASCIPlotter(int canvas_width, int canvas_height) : m_canvas_width(canvas_width),
-                                                                m_canvas_height(canvas_height) {
+ASCIPlotter::ASCIPlotter(std::string list_of_used_characters, 
+                         int canvas_width,
+                         int canvas_height) : m_used_symbols(list_of_used_characters),
+                                              m_canvas_width(canvas_width),
+                                              m_canvas_height(canvas_height) {
 
     m_elements_number = std::pow(MAX_ARRAY_SIZE, 2);
-
     std::memset(m_output, ' ', m_elements_number);
-    std::memset(m_zbuffer, 0., 8 * m_elements_number);
+
+    m_idx_coeff = int (list_of_used_characters.length() / std::sqrt(2));
 
     m_light_direction.normalize();
 
@@ -15,9 +18,7 @@ ASCIPlotter::ASCIPlotter(int canvas_width, int canvas_height) : m_canvas_width(c
 
 void ASCIPlotter::provide_data_3d(const Eigen::MatrixXd &data_3d) {
 
-    m_plotting_data = data_3d;
-
-    // compute_projection(m_plotting_data);
+    // compute_projection(data_3d);
 
 }
 
@@ -25,25 +26,39 @@ void ASCIPlotter::compute_projection(const Eigen::MatrixXd &data_3d, double x_sc
                                                                      double y_scale,
                                                                      double depth_offset) {
     
-    m_ooz = 1. / (data_3d.col(2).array() + depth_offset);
+    Eigen::VectorXi x_projection, y_projection, luminance_idx;
+    Eigen::VectorXd x, y, z, ooz;
 
-    m_x_proj = (m_canvas_width / 2 +  
-               x_scale * m_ooz.array() * data_3d.col(0).array()).cast<int>();
+    x = data_3d.col(0);
+    y = data_3d.col(1);
+    z = data_3d.col(2);
 
-    m_y_proj = (m_canvas_height / 2 -  
-               y_scale * m_ooz.array() * data_3d.col(1).array()).cast<int>();
+    /* Compute one over zed */
+    ooz = 1. / (z.array() + depth_offset);
 
+    /* Compute projections */
+    x_projection = (m_canvas_width / 2 +  
+                    x_scale * ooz.array() * x.array()).cast<int>();
+    y_projection = (m_canvas_height / 2 -  
+                    y_scale * ooz.array() * y.array()).cast<int>();
+
+    /* Compute luminance indexis for each point */
+    luminance_idx = (data_3d.rowwise().normalized() * m_light_direction * m_idx_coeff).cast<int>();
+
+    int symbol_idx = 0;
     for (int i = 0; i < data_3d.rows(); i ++) {
+        
+        if (luminance_idx(i) > 0) {
 
-        double luminance = m_light_direction.dot(data_3d.row(i).normalized());
-        int luminance_idx = luminance * 8;
+            symbol_idx = luminance_idx(i);
 
-       if (m_ooz(i) > m_zbuffer[m_x_proj(i)][m_y_proj(i)]) {
-            
-            m_zbuffer[m_x_proj(i)][m_y_proj(i)] = m_ooz(i);
-            m_output[m_x_proj(i)][m_y_proj(i)] = ".,-~:;=!*#$@"[luminance_idx > 0 ? luminance_idx : 0];
+        } else {
 
-       }
+            symbol_idx = 0;
+
+        }
+
+        m_output[x_projection(i)][y_projection(i)] = m_used_symbols[symbol_idx];
 
     }
 
@@ -54,6 +69,7 @@ void ASCIPlotter::plot_asci() {
     /* Clear current console output */
     std::printf("\x1b[H");
 
+    /* Output every buffer element to console */
     for (int height_idx = 0; height_idx < m_canvas_height; height_idx ++) {
 
         for (int width_idx = 0; width_idx < m_canvas_width; width_idx ++) {
@@ -70,14 +86,39 @@ void ASCIPlotter::plot_asci() {
 
 }
 
+void ASCIPlotter::create_rotation_matrix(double alpha, double beta, double gamma) {
+
+    double sinalpha = std::sin(alpha), sinbeta = std::sin(beta), singamma = std::sin(gamma);
+    double cosalpha = std::cos(alpha), cosbeta = std::cos(beta), cosgamma = std::cos(gamma);
+
+    Eigen::Matrix3d Rx, Ry, Rz;
+
+    Rx << 1.,     0.,       0.,
+          0., cosalpha, -sinalpha,
+          0., sinalpha,  cosalpha;
+
+    Ry << cosbeta, 0., -sinbeta,
+             0.,   1.,     0,
+          sinbeta, 0.,  cosbeta;
+
+    Rz << cosgamma, -singamma, 0.,
+          singamma,  cosgamma, 0.,
+             0.,        0.,    1.;
+    
+    m_R3d = Rx * Ry * Rz;
+
+}
+
 Eigen::MatrixXd ASCIPlotter::rotate3d(const Eigen::MatrixXd &data_3d) {
 
-    return data_3d;
+    Eigen::MatrixXd rotated_data = data_3d * m_R3d;
+    return rotated_data;
 
 }
 
 Eigen::MatrixXd ASCIPlotter::rotate2d(const Eigen::MatrixXd &data_2d) {
 
-    return data_2d;
+    Eigen::MatrixXd rotated_data = data_2d * m_R2d;
+    return rotated_data;
 
 }
